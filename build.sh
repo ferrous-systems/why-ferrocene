19c0dacfa16e2ee07d14c09be830014ef2f764b2
Jonathan Pallant (Ferrous Systems) <jonathan.pallant@ferrous-systems.com>
2023-09-11 17:35:57 +0100

    Test the book.

--- build.sh
+++ build.sh
@@ -3,8 +3,16 @@
 set -euo pipefail
 
 rm -rf ./slides || true
+rm -rf ./book || true
 mkdir ./slides
 mkdir ./slides/images
-mdslides --output-dir ./slides --template ./template.html --index-template ./index-template.html
-cp ./src/images/*.png ./slides/images
+# Check the examples are OK
+mdbook test
+# book goes into ./book
 mdbook build
+# slides go into ./slides
+mdslides \
+    --output-dir ./slides \
+    --template ./template.html \
+    --index-template ./index-template.html
+cp ./src/images/*.png ./slides/images
